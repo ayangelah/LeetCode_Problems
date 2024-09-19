08de03ce37d57ca8c9a0a964d0dc730f8ebec3e0
ayang007 <86279159+ayang007@users.noreply.github.com>
2024-09-18 21:58:10 -0700

    9/18/24

---
+++ easy/26remove_duplicate_from_sorted_array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
+        i = 0
+        offsets = [0] * len(nums)
+        while i < len(nums):
+            j = 1
+            while i+j < len(nums) and nums[i+j] == nums[i]: # absorb duplicates
+                offsets[i] += 1
+                j += 1
+            i += max(1, offsets[i])
+        list_shift = 0
+        for index, offset in enumerate(offsets):
+            if offset != 0:
+                del nums[index-list_shift:index+offset-list_shift]
+                list_shift += offset
+        return len(nums)
